86311a66b39aeeb99e2d8e115347772666f5a48b
xyh11223 <xyh11223@noah-fund.com>
2017-08-09 15:20:10 +0800

    tkhello3 example

---
+++ tkhello3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import Tkinter
+top = Tkinter.Tk()
+
+hello = Tkl
+quit = Tkinter.Button(top, text = 'Hello World!',
+        command = top.quit)        
+quit.pack()
+Tkinter.mainloop()
