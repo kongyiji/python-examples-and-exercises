9b4c9ffb05ee510328d8c92cf2b188a121f736fa
xyh11223 <xyh11223@noah-fund.com>
2017-09-01 17:31:00 +0800

    ex

---
+++ CorePythonProgramming_Exercises/2-5a.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# platform: python3
+
+i = 0
+while i <= 10:
+    print (i)
+    i += 1
---
+++ CorePythonProgramming_Exercises/2-5b.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# platform: python3
+
+for i in range(11):
+    print (i)
