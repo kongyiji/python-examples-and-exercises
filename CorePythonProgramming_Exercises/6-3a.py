4db6b32b4ac7d04391d3c543c8b46e256cf6ce9f
Kong Yiji <>
2017-09-11 17:53:09 +0800

    ex

--- CorePythonProgramming_Exercises/6-3a.py
+++ CorePythonProgramming_Exercises/6-3a.py
@@ -4,13 +4,30 @@
 
 import string
 
+def large_to_small(a, b):
+    if a < b:
+        a, b = b, a
+    return a, b
+
 nums = string.digits
 
 inputNumber = input('Please input numbers:')
+lenInput = len(inputNumber)
 
 if len(inputNumber) >= 1:
 
-    for s in inputNumber[:]:
+    numList = []
+
+    for num in inputNumber[:]:
         if num not in nums:
             print('invalid: Please input only number.')
             break
+
+        numList.append(num)
+
+    for num in numList:
+        print(num, end='')
+    numList.sort()
+    print()
+    for num in numList:
+        print(num, end='')
